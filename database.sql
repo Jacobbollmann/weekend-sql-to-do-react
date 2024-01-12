f8c58ec215d4d9861d4a580292f3f67522fea79e
Jacob Bollmann <jacob.bollmann@yahoo.com>
2024-01-11 20:25:33 -0600

    Added sql table and GET route working

---
+++ database.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "todo" (
+  "id" SERIAL PRIMARY KEY, 
+  "task" VARCHAR (100) NOT NULL,
+  "description" VARCHAR (250),
+  "completed" BOOLEAN DEFAULT FALSE
+);
+
+INSERT INTO "todo" ("task", "description", "completed")
+VALUES 
+('Clean house', 'Living room needs most attention', FALSE),
+('Go to post office', 'Dont forget to get stamps while there', FALSE).
+('Homework due', 'Just one assignment this week', FALSE);
--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,7 +3,20 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // GET
+router.get('/', (req, res) => {
+  const dbQuery = 'SELECT * FROM "todo";';
 
+  pool
+  .query(dbQuery)
+  .then((result) => {
+    console.log('In Get Route;');
+    res.send(result.rows).status(201);   
+  })
+  .catch((error) => {
+    console.error('ERROR', error);
+    res.send('Something wrong it GET:').status(500);
+  });
+});
 // POST
 
 // PUT
