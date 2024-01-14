102511f54a7c868397cc47a573c3a3fe2e57e550
Jacob Bollmann <jacob.bollmann@yahoo.com>
2024-01-14 16:43:32 -0600

    Route API folder made and GET route sending data from database completed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,31 @@
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
+import './App.css';
+import { fetchTasks } from './taskApi/taskApi';
 
 function App () {
-  
+  const [taskList, setTaskList] = useState();
+
+  const refreshTask = () => {
+    const taskPromise = fetchTasks();
+
+    taskPromise
+      .then((response) => {
+        console.log('DATA FROM GET:', response.data);
+      })
+      .catch((error) => {
+        console.error('SOMETING WRONG IN GET:', error);
+        alert('SOMETHING WENT WRONG. CHECK CONSOLE');
+      });
+  };
+
+  useEffect(() => {
+    console.log('Initial Page Load');
+    refreshTask();
+  }, []);
+
   return (
-    <div>
-      <h1>TO DO APP</h1>
+    <div className='App'>
+      <h1>JUST DO IT</h1>
     </div>
   );
 
