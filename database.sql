7ce1b77108f183014c356741b235533ee2dc6f83
Jacob Bollmann <jacob.bollmann@yahoo.com>
2024-02-06 20:45:52 -0600

    accounting for db deployment URL

--- database.sql
+++ database.sql
@@ -8,5 +8,5 @@ CREATE TABLE "todo" (
 INSERT INTO "todo" ("task", "description", "completed")
 VALUES 
 ('Clean house', 'Living room needs most attention', FALSE),
-('Go to post office', 'Dont forget to get stamps while there', FALSE).
+('Go to post office', 'Dont forget to get stamps while there', FALSE),
 ('Homework due', 'Just one assignment this week', FALSE);
